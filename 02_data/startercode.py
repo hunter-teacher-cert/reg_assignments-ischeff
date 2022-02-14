f1f061e9b1c67acf84a18873e2c62feb7056d499
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-02-13 23:15:02 -0500

    starting code

---
+++ 02_data/startercode.py
@@ -0,0 +1,9 @@
+import csv
+
+def population_center(data):
+    #open the csv file
+    #skip the first line (the headers)
+    #sum all the latitudes and longitudes and average them to find the center! 
+
+if __name__ == "__main__":
+    #call your function here!
