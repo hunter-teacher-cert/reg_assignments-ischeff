fec24f6b1cd66a7aeb6825e9e8affd21d256a8b9
Ian Scheffler <Ian.f.scheffler@gmail.com>
2022-02-13 23:01:11 -0500

    Update data.py

--- 02_data/data.py
+++ 02_data/data.py
@@ -1,10 +1,17 @@
 import csv
 
-def load_input(data):
-    file = open('nba.csv')
-    reader = csv.reader(file)
-    data = list(reader)
-    print(data)
+def population_center(data):
+    with open(data, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        x, y, counter = 0, 0, 0
+        for row in reader:
+            x += float(row[0])
+            y += float(row[1])
+            counter += 1
+        longitude = x / counter
+        latitude = y / counter
+        print(f"The center of the squirrel population is at {longitude}, {latitude}")
 
 if __name__ == "__main__":
-    load_input('nba.csv')
+    population_center('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
