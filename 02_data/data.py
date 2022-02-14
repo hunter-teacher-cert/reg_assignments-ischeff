e68d46fe061910350729226df1454c9160742d52
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-02-13 21:29:31 -0500

    test

---
+++ 02_data/data.py
@@ -0,0 +1,20 @@
+#~~~~~~~~~~~~file read using csv.reader~~~~~~~~~~~~~~~~
+import csv
+
+# def load_input(data):
+#     with open(data) as f:
+#         depths = [int(i) for i in f]
+#         return
+#~~~~~~~~~~~file read using with() statement~~~~~~~~~~~
+
+
+
+
+#~~~~~~~~~~~~file read using .readlines() method~~~~~~~~~~~~
+
+
+
+#~~~~~~~~~~file read using pandas~~~~~~~~~~~~~~~~~~~~~
+
+if __name__ == "__main__":
+    print("hello, world!")
